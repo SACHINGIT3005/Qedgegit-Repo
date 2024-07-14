578bc1bfc190cbd71cf25e5f03575d957a2dc765
SACHINGIT3005 <sachinsherapur3005@gmail.com>
2024-07-14 22:43:17 +0530

    feature201 changes in application.java file by vinay

--- Application.java
+++ Application.java
@@ -15,6 +15,22 @@ class Main {
         System.out.println("The file already exists.");
       }
     }
+#feature201 changesby vinay for module payment gateway in july 2024
+public static void main(String[] args) {
+
+    char[] array = new char[100];
+    try {
+      // Creates a reader using the FileReader
+      FileReader input = new FileReader("input.txt");
+
+      // Reads characters
+      input.read(array);
+      System.out.println("Data in the file:");
+      System.out.println(array);
+
+      // Closes the reader
+      input.close();
+#End of feature201 code
     catch(Exception e) {
       e.getStackTrace();
     }
